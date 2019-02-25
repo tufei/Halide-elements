bed210d6e4166a1c3f408358761d592f73e7c635
Ngyu-Phee Yen <yyuan@gmx.com>
2019-02-24 16:29:51 -0800

    Extended and fixed the building of the 'cmpgt' element.

--- src/cmpgt/cmpgt_generator.cc
+++ src/cmpgt/cmpgt_generator.cc
@@ -4,28 +4,27 @@
 #include "Element.h"
 
 using namespace Halide;
-using Halide::Element::schedule;
+using namespace Halide::Element;
 
 template<typename T>
 class Cmpgt : public Halide::Generator<Cmpgt<T>> {
 public:
-    ImageParam src0{type_of<T>(), 2, "src0"};
-    ImageParam src1{type_of<T>(), 2, "src1"};
+  GeneratorInput<Buffer<T>> src0{"src0", 3};
+  GeneratorInput<Buffer<T>> src1{"src1", 3};
 
-    GeneratorParam<int32_t> width{"width", 1024};
-    GeneratorParam<int32_t> height{"height", 768};
+  GeneratorParam<int32_t> width{"width", 1024};
+  GeneratorParam<int32_t> height{"height", 768};
+  GeneratorParam<int32_t> depth{"depth", 3};
 
-    Func build() {
-        Func dst{"dst"};
+  GeneratorOutput<Buffer<T>> dst{"dst", 3};
 
-        dst = Element::cmpgt<T>(src0, src1);
+  void generate() {
+    dst = cmpgt<T>(src0, src1);
 
-        schedule(src0, {width, height});
-        schedule(src1, {width, height});
-        schedule(dst, {width, height});
-
-        return dst;
-    }
+    schedule(src0, {width, height, depth});
+    schedule(src1, {width, height, depth});
+    schedule(dst, {width, height, depth});
+  }
 };
 
 HALIDE_REGISTER_GENERATOR(Cmpgt<uint8_t>, cmpgt_u8);
