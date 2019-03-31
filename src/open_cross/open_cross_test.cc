24e68d37b7e6d0b7a8a101abceeba46d3b6853f1
Ngyu-Phee Yen <yyuan@gmx.com>
2019-03-30 19:49:23 -0700

    Extended and fixed building of the 'open_cross' element.

--- src/open_cross/open_cross_test.cc
+++ src/open_cross/open_cross_test.cc
@@ -14,25 +14,27 @@
 
 // returns index of result workbuf
 template<typename T>
-int conv_cross(int width, int height, int window_width, int window_height, int iteration,
+int conv_cross(int width, int height, int depth, int window_width, int window_height, int iteration,
               T* workbuf, const T&(*f)(const T&, const T&), T init, int k) {
 
     int itr;
     for (itr=k; itr<k+iteration; ++itr) {
-        for (int y=0; y<height; ++y) {
-            for (int x=0; x<width; ++x) {
-                T minx = init, miny = init;
-                for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
-                    int yy = y + j >= 0 ? y + j: 0;
-                    yy = yy < height ? yy : height - 1;
-                    miny = f(miny, workbuf[(itr%2)*width*height + x*height + yy]);
+        for (int c=0; c<depth; ++c) {
+            for (int y=0; y<height; ++y) {
+                for (int x=0; x<width; ++x) {
+                    T minx = init, miny = init;
+                    for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
+                        int yy = y + j >= 0 ? y + j: 0;
+                        yy = yy < height ? yy : height - 1;
+                        miny = f(miny, workbuf[(itr%2)*width*height*depth + (x*height + yy)*depth + c]);
+                    }
+                    for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
+                        int xx = x + i >= 0 ? x + i: 0;
+                        xx = xx < width ? xx : width - 1;
+                        minx = f(minx, workbuf[(itr%2)*width*height*depth + (xx*height + y)*depth + c]);
+                    }
+                    workbuf[((itr+1)%2)*width*height*depth + (x*height + y)*depth + c] = f(minx, miny);
                 }
-                for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
-                    int xx = x + i >= 0 ? x + i: 0;
-                    xx = xx < width ? xx : width - 1;
-                    minx = f(minx, workbuf[(itr%2)*width*height + xx*height + y]);
-                }
-                workbuf[((itr+1)%2)*width*height + x*height + y] = f(minx, miny);
             }
         }
     }
@@ -50,41 +52,48 @@ int test(int (*func)(struct halide_buffer_t *_src_buffer, struct halide_buffer_t
         //
         const int width = 1024;
         const int height = 768;
+        const int depth = 3;
         const int window_width = 3;
         const int window_height = 3;
         const int iteration = 2;
-        const std::vector<int32_t> extents{width, height};
+        const std::vector<int32_t> extents{width, height, depth};
         auto input = mk_rand_buffer<T>(extents);
         auto output = mk_null_buffer<T>(extents);
-        T (*expect)[width][height], workbuf[2][width][height];
+        T *tmp = new T[2*width*height*depth];
+        T (*expect)[width][height][depth];
+        T (*workbuf)[width][height][depth] = reinterpret_cast<T (*)[width][height][depth]>(tmp);
 
-        for (int y=0; y<height; ++y) {
-            for (int x=0; x<width; ++x) {
-                workbuf[0][x][y] = input(x, y);
+        for (int c=0; c<depth; ++c) {
+            for (int y=0; y<height; ++y) {
+                for (int x=0; x<width; ++x) {
+                    workbuf[0][x][y][c] = input(x, y, c);
+                }
             }
         }
 
         // erode
-        int k = conv_cross(width, height, window_width, window_height, iteration,
-                           &workbuf[0][0][0], static_cast<const T&(*)(const T&, const T&)>(std::min), std::numeric_limits<T>::max(), 0);
+        int k = conv_cross(width, height, depth, window_width, window_height, iteration,
+                           &workbuf[0][0][0][0], static_cast<const T&(*)(const T&, const T&)>(std::min), std::numeric_limits<T>::max(), 0);
         // dilate
-        k = conv_cross(width, height, window_width, window_height, iteration,
-                       &workbuf[0][0][0], static_cast<const T&(*)(const T&, const T&)>(std::max), std::numeric_limits<T>::min(), k);
+        k = conv_cross(width, height, depth, window_width, window_height, iteration,
+                       &workbuf[0][0][0][0], static_cast<const T&(*)(const T&, const T&)>(std::max), std::numeric_limits<T>::min(), k);
 
 
         expect = &(workbuf[k%2]);
 
         func(input, output);
 
-        for (int y=0; y<height; ++y) {
-            for (int x=0; x<width; ++x) {
-                T actual = output(x, y);
-                if ((*expect)[x][y] != actual) {
-                    throw std::runtime_error(format("Error: expect(%d, %d) = %d, actual(%d, %d) = %d\n", x, y, (*expect)[x][y], x, y, actual));
+        for (int c=0; c<depth; ++c) {
+            for (int y=0; y<height; ++y) {
+                for (int x=0; x<width; ++x) {
+                    T actual = output(x, y, c);
+                    if ((*expect)[x][y][c] != actual) {
+                        throw std::runtime_error(format("Error: expect(%d, %d, %d) = %d, actual(%d, %d, %d) = %d\n", x, y, c, (*expect)[x][y][c], x, y, c, actual));
+                    }
                 }
             }
         }
-
+        delete[] tmp;
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
