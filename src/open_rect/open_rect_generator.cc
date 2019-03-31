1c862fda9b673ea9dc60a7876e393b63a1f54f8a
Ngyu-Phee Yen <yyuan@gmx.com>
2019-03-31 10:14:25 -0700

    Extended and fixed building of the 'open_rect' element.

--- src/open_rect/open_rect_generator.cc
+++ src/open_rect/open_rect_generator.cc
@@ -4,30 +4,31 @@
 #include "Element.h"
 
 using namespace Halide;
-using Halide::Element::schedule;
+using namespace Halide::Element;
 
 template<typename T>
 class OpenRect : public Halide::Generator<OpenRect<T>> {
 public:
-    ImageParam src{type_of<T>(), 2, "src"};
+    GeneratorInput<Buffer<T>> src{"src", 3};
 
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
+    GeneratorParam<int32_t> depth{"depth", 3};
     GeneratorParam<int32_t> iteration{"iteration", 2};
     GeneratorParam<int32_t> window_width{"window_width", 3, 3, 17};
     GeneratorParam<int32_t> window_height{"window_height", 3, 3, 17};
 
-    Func build() {
-        Func erode{"erode"}, dilate{"dilate"};
+    GeneratorOutput<Buffer<T>> dilated{"dilated", 3};
 
-        erode = Element::erode_rect<T>(src, width, height, window_width, window_height, iteration);
-        dilate = Element::dilate_rect<T>(erode, width, height, window_width, window_height, iteration);
+    void generate() {
+        Func eroded{"eroded"};
 
-        schedule(src, {width, height});
-        schedule(erode, {width, height});
-        schedule(dilate, {width, height});
+        eroded = erode_rect<T>(src, width, height, depth, window_width, window_height, iteration);
+        dilated = dilate_rect<T>(eroded, width, height, depth, window_width, window_height, iteration);
 
-        return dilate;
+        schedule(src, {width, height, depth});
+        schedule(eroded, {width, height, depth});
+        schedule(dilated, {width, height, depth});
     }
 };
 
