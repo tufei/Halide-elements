71d766fc5ef0b2c099cfef35c6ff3af2ed457bf1
Ngyu-Phee Yen <yyuan@gmx.com>
2019-04-07 10:30:11 -0700

    Extended and fixed building of the 'sincos' element.

--- src/sincos/sin_cos_generator.cc
+++ src/sincos/sin_cos_generator.cc
@@ -7,25 +7,24 @@ using namespace Halide;
 using namespace Halide::Element;
 
 class SinCos : public Halide::Generator<SinCos> {
-    ImageParam src{type_of<float>(), 2, "src"};
+public:
+    GeneratorInput<Buffer<float>> src{"src", 3};
 
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
+    GeneratorParam<int32_t> depth{"depth", 3};
 
-public:
-    Func build() {
-        Func dst{"dst"};
-        Var x, y;
+    GeneratorOutput<Buffer<float>> dst{"dst", 3};
+
+    void generate() {
+        Var x, y, c;
         FixedN<30, 28, true> e =
-            select((x + y) % 2 == 0,
-                   Element::sin(to_fixed<30, 25>(src(x, y))),
-                   Element::cos(to_fixed<30, 25>(src(x, y))));
-        dst(x, y) = from_fixed<float>(e);
+            select((x + y) % 2 == 0, sin(to_fixed<30, 25>(src(x, y, c))),
+                   cos(to_fixed<30, 25>(src(x, y, c))));
+        dst(x, y, c) = from_fixed<float>(e);
 
-        schedule(src, {width, height});
-        schedule(dst, {width, height});
-
-        return dst;
+        schedule(src, {width, height, depth});
+        schedule(dst, {width, height, depth});
     }
 };
 
--- src/sincos/sin_cos_test.cc
+++ src/sincos/sin_cos_test.cc
@@ -22,26 +22,28 @@ int test(int (*func)(struct halide_buffer_t *_src_buffer1, struct halide_buffer_
         //
         const int width = 1024;
         const int height = 768;
-        const std::vector<int32_t> extents{width, height};
+        const int depth = 3;
+        const std::vector<int32_t> extents{width, height, depth};
         auto input = mk_rand_real_buffer<float>(extents, 0, 1);
         auto output = mk_null_buffer<float>(extents);
 
         func(input, output);
 
         double diff_max = 0.0;
-        for (int y=0; y<height; ++y) {
-            for (int x=0; x<width; ++x) {
-                float expect = (x + y) % 2 == 0 ? sin(input(x, y)) : cos(input(x, y));
-                float actual = output(x, y);
-                double diff = fabs(expect - actual);
-                diff_max = std::max(diff, diff_max);
-                if (diff > 0.0000001) {
-                    throw std::runtime_error(format("Error: expect: %s(%.10lf) = %.10lf, actual: %s(%.10lf) = %.10lf, diff = %.10lf",
-                                                    (x + y) % 2 == 0 ? "sin" : "cos", input(x, y), expect,
-                                                    (x + y) % 2 == 0 ? "sin" : "cos", input(x, y), actual, diff).c_str());
+        for (int c=0; c<depth; ++c) {
+            for (int y=0; y<height; ++y) {
+                for (int x=0; x<width; ++x) {
+                    float expect = (x + y) % 2 == 0 ? sin(input(x, y, c)) : cos(input(x, y, c));
+                    float actual = output(x, y, c);
+                    double diff = fabs(expect - actual);
+                    diff_max = std::max(diff, diff_max);
+                    if (diff > 0.0000001) {
+                        throw std::runtime_error(format("Error: expect: %s(%.10lf) = %.10lf, actual: %s(%.10lf) = %.10lf, diff = %.10lf",
+                                                        (x + y) % 2 == 0 ? "sin" : "cos", input(x, y, c), expect,
+                                                        (x + y) % 2 == 0 ? "sin" : "cos", input(x, y, c), actual, diff).c_str());
+                    }
                 }
             }
-
         }
         std::cout <<  "Max diff = " << std::setprecision(17) << diff_max << std::endl;
 
