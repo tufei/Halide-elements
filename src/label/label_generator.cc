88e1231a5325d4793958825f5daeb1e8a3345287
Ngyu-Phee Yen <yyuan@gmx.com>
2021-01-09 17:40:46 -0800

    Updated auto-scheduling for the 'label' element.

--- src/label/label_generator.cc
+++ src/label/label_generator.cc
@@ -13,33 +13,18 @@ public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
 
-#if 1
-    GeneratorOutput<Buffer<uint32_t>> dst0{"dst0", 2};
-    GeneratorOutput<Buffer<uint32_t>> dst1{"dst1", 2};
-#else
     GeneratorOutput<Buffer<>> dst{"dst", {UInt(32), UInt(32)}, 2};
-#endif
 
     void generate() {
-#if 1
-        Var x{"x"}, y{"y"};
-
-        dst0(x, y) =
-            label_firstpass<T>(src, width, height,
-                               this->auto_schedule)(x, y)[0];
-        dst1(x, y) =
-            label_firstpass<T>(src, width, height,
-                               this->auto_schedule)(x, y)[1];
-#else
         dst = label_firstpass<T>(src, width, height, this->auto_schedule);
-#endif
     }
 
     void schedule() {
         if (this->auto_schedule) {
             src.set_estimates({{0, 1024}, {0, 768}});
-            dst0.set_estimates({{0, 1024}, {0, 768}});
-            dst1.set_estimates({{0, 1024}, {0, 768}});
+            for (auto &buffer : static_cast<Func>(dst).output_buffers()) {
+                buffer.set_estimates({{0, 1024}, {0, 768}});
+            }
         } else {
             ::schedule(src, {width, height});
         }
